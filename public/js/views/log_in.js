f5edeeaeb36e53a2f54dc84022c575137757d2ad
Robert Pearce <robertwaltonpearce@gmail.com>
2013-06-08 13:52:15 -0400

    refactored log in

--- public/js/views/log_in.js
+++ public/js/views/log_in.js
@@ -2,63 +2,69 @@ BikeCheck.Views.LogIn = Parse.View.extend({
   className: 'login',
   template: _.template($('#login-tmpl').html()),
   events: {
-    "submit form.login-form": "logIn",
-    "submit form.signup-form": "signUp"
+    'submit form.login-form': 'logIn',
+    'submit form.signup-form': 'signUp'
   },
 
   initialize: function() {
-    _.bindAll(this, "logIn", "signUp");
+    _.bindAll(this, 'logIn', 'signUp');
   },
 
   render: function() {
     this.$el.html(this.template());
-    this.delegateEvents();
+    // this.delegateEvents();
     return this;
   },
 
-  logIn: function(e) {
-    // e.preventDefault();
-    var self = this,
-        username = this.$("#login-username").val(),
-        password = this.$("#login-password").val();
+  getLoginCreds: function(credsType) {
+    var creds = {
+      username: this.$('#' + credsType + '-username').val(),
+      password: this.$('#' + tcredsTypeype + '-password').val()
+    };
+    return creds;
+  },
 
-    Parse.User.logIn(username, password, {
+  attemptLogIn: function(creds) {
+    return Parse.User.logIn(creds.username, obj.password, {
       success: function(user) {
         console.log('success!');
         // load some new view!
-        self.undelegateEvents();
-        delete self;
+        // self.undelegateEvents();
+        // delete self;
      },
       error: function(user, error) {
-        self.$(".login-form .error").html("Invalid username or password. Please try again.").show();
-        self.$(".login-form button").removeAttr("disabled");
+        self.$('.login-form .error').html('Invalid username or password. Please try again.').show();
+        self.$('.login-submit').removeAttr('disabled');
       }
     });
+  },
 
-    this.$(".login-form button").attr("disabled", "disabled");
-    return false;
+  attemptSignUp: function(creds) {
+    return Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
+      success: function(user) {
+        // load some new view!
+        console.log('success!');
+        // self.undelegateEvents();
+        // delete self;
+      },
+      error: function(user, error) {
+        self.$('.signup-form .error').html(error.message).show();
+        self.$('.signup-submit').removeAttr('disabled');
+      }
+    });
+  },
+
+  logIn: function(e) {
+    e.preventDefault();
+    var creds = this.getLoginCreds('login');
+    this.attemptLogIn(creds);
+    return this.$('.login-submit').attr('disabled', 'disabled');
   },
 
   signUp: function(e) {
-    // e.preventDefault();
-    var self = this,
-        username = this.$("#signup-username").val(),
-        password = this.$("#signup-password").val();
-
-    Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
-      success: function(user) {
-        // load some new view!
-        console.log('success!');
-        self.undelegateEvents();
-        delete self;
-      },
-      error: function(user, error) {
-        self.$(".signup-form .error").html(error.message).show();
-        self.$(".signup-form button").removeAttr("disabled");
-      }
-    });
-
-    this.$(".signup-form button").attr("disabled", "disabled");
-    return false;
+    e.preventDefault();
+    var creds = this.getLoginCreds('signup');
+    this.attemptSignUp(creds);
+    return this.$('.signup-submit').attr('disabled', 'disabled');
   }
 });
