ff8a96705edfb95f326ced0b6af7ad799c725fee
Chetan Shenoy <me@chetanshenoy.com>
2013-06-08 20:18:19 -0400

    adding in marker models

--- public/js/views/map.js
+++ public/js/views/map.js
@@ -1,22 +1,75 @@
 BikeCheck.Views.Map = Parse.View.extend({
   id: 'map-canvas',
   events: {
+
   },
 
   initialize: function(){
-  	console.log('this.id', this.id);
-  	var mapOptions = {
+  	_.bindAll(this, 'add', 'addMarker');
+  	this.render();
+  },
+
+  render: function(){
+  	this.mapOptions = {
 	    center: new google.maps.LatLng(38.8935965, -77.014576),
 	    zoom: 13,
 	    mapTypeId: google.maps.MapTypeId.ROADMAP
 	  };
 	  
-	  var map = new google.maps.Map(document.getElementById(this.id), mapOptions);
+	  this.map = new google.maps.Map(document.getElementById(this.id), this.mapOptions);
 
-		// google.maps.event.addDomListener(window, 'load', initialize);
+	  // Add Marker Btn
+	  this.markerBtn = document.createElement('div');
+	  this.markerBtn.style.padding = '5px';
+	  this.markerBtn.style.margin = '5px';
+
+	  var insideBtn = document.createElement('div');
+	  insideBtn.style.borderWidth = '1px';
+	  insideBtn.style.borderStyle = 'solid';
+	  insideBtn.style.borderColor = 'black';
+	  insideBtn.style.backgroundColor = 'white';
+		insideBtn.style.cursor = 'pointer';
+		this.markerBtn.appendChild(insideBtn);
+
+		var controlText = document.createElement('div');
+		controlText.style.fontFamily = 'Arial,sans-serif';
+	  controlText.style.fontSize = '12px';
+	  controlText.style.paddingLeft = '4px';
+	  controlText.style.paddingRight = '4px';
+	  controlText.innerHTML = 'Add Marker!';
+	  insideBtn.appendChild(controlText);
+
+	  this.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(this.markerBtn);
+
+	  google.maps.event.addDomListener(this.markerBtn, 'click', this.add);
+
+		return this;
   },
 
-  render: function(){
-  	console.log('render');
+  add: function(){
+  	this.map.setOptions({draggableCursor:'crosshair'});
+  	google.maps.event.addDomListenerOnce(this.map, 'click', this.addMarker);
+  },
+
+  addMarker: function(loc){
+  	loc = loc.latLng; // loc grabs just lat-long coords
+  	var blig = this.map,
+  			marker,
+  			newBikeEvent;
+  	
+  	marker = new google.maps.Marker({
+      position: loc,
+      map: blig
+    });
+ 		
+ 		newBikeEvent = new BikeCheck.Models.BikeEvent();
+ 		newBikeEvent.save({
+ 			"location": loc
+ 		}, {
+ 			success: function(newBikeEvent){
+ 				console.log("SUCCESS! ", newBikeEvent);
+ 			}
+ 		});
+    this.map.setOptions({draggableCursor:'pointer'});
   }
 });
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ routes = function(app){
 
 	// Home page with map full screen
 	app.get('/', function(req, res){
-		console.log(req.headers);
 		res.render('index', {
 			title: 'BikeCheck',
 			map: true
