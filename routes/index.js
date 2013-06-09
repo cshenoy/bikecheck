80190b2fbf0554fc390d91109f6025fe11cec999
Chetan Shenoy <me@chetanshenoy.com>
2013-06-08 21:48:42 -0400

    adding parse api into server side

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 // index.js
+var Kaiseki = require('kaiseki');
 
+// instantiate
+var APP_ID = 'JfnQBEqS47BirgQbg6LrlLzTBndhqWlOLU7p74zt';
+var REST_API_KEY = 'Ei57rw4ouA30Zk5Vg5jYGX2F2uVLiQFZ2CQfUird';
+var kaiseki = new Kaiseki(APP_ID, REST_API_KEY);
 
 routes = function(app){
 
@@ -9,6 +14,23 @@ routes = function(app){
 			title: 'BikeCheck',
 			map: true
 		});
+	});
+
+	app.get('/json/all', function(req, res){
+		kaiseki.getObjects('BikeEvent', function(err, res, body, success){
+			console.log(body);
+			printAll(body, success);
+		});
+
+		function printAll(b, bool){
+			if(bool) res.json(b);
+			else {
+				res.json({});
+			}
+		}
+	});
+
+	app.get('/newBikeEvent', function(req, res){
 	});
 };
 
